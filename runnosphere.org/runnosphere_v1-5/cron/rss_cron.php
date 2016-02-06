62a40d6eb276b6bb72f2297374b760507e38b8ef
Djailla <bastien.vallet@gmail.com>
2016-02-06 14:28:50 +0100

    Cleanup RSS code

--- runnosphere.org/runnosphere_v1-5/cron/rss_cron.php
+++ runnosphere.org/runnosphere_v1-5/cron/rss_cron.php
@@ -8,13 +8,13 @@
 		'orderby' => 'login',
 		'order' => 'ASC',
 		'fields' => 'all'
-	 );
+	);
 
-	 $users = get_users($args);
-	 foreach($users as $usr){
-		 $uid = $usr->ID;
+	$users = get_users($args);
+	foreach($users as $usr){
+		$uid = $usr->ID;
 
-		 if(get_user_meta($uid, "rss_active", true) == "1"){
+		if(get_user_meta($uid, "rss_active", true) == "1"){
 
 			//Admin enable this RSS
 			$rss = get_user_meta($uid, "rss_address", true);
@@ -31,7 +31,7 @@
 						$path = dirname(__FILE__)."/xml_v2/rss-".$uid.".xml";
 
 						file_put_contents($path, "");
-						file_put_contents($path,file_get_contents($flux));
+						file_put_contents($path, file_get_contents($flux));
 
 						$ok[$uid] = "ok";
 				}
@@ -39,9 +39,6 @@
 		}
 	}
 
-	$new_array = array_map(create_function('$key, $value', 'return $key.":".$value." # ";'), array_keys($ok), array_values($ok));
-	mail("bastien.vallet@gmail.com","Runno RSS : CRON", "RESULT  : ".implode($new_array));
-
-	print_r($ok);
-
+	$new_array = array_map(create_function('$key, $value', 'return $key.": ".$value;'), array_keys($ok), array_values($ok));
+	mail("bastien.vallet@gmail.com", "Runno RSS : CRON", "RESULT  :\n".implode("\n", $new_array));
 ?>
