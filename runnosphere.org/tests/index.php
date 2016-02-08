8bf9c6e2da927d4d65f209ca530298e2bf8eb4fe
Djailla <bastien.vallet@gmail.com>
2016-02-08 10:55:45 +0100

    Add tests for RSS issue

---
+++ runnosphere.org/tests/index.php
@@ -0,0 +1,32 @@
+<?php
+
+	libxml_use_internal_errors(true);
+
+	$rss = "http://www.jerem-runner.com/rss";
+
+	if(!empty($rss)){
+		$flux = stripslashes($rss);
+		$error = 0;
+
+		print_r($flux);
+
+		if(!@$fluxrss=simplexml_load_file($flux, 'SimpleXMLElement', LIBXML_NOCDATA)){
+			    $errors = libxml_get_errors();
+			    print_r($errors);
+
+				$error = 1;
+				$ok[$uid] = "error : ".$flux;
+		}
+
+		if($error == 0){
+				$path = dirname(__FILE__)."/xml_v2/rss-".$uid.".xml";
+
+				file_put_contents($path, "");
+				file_put_contents($path, file_get_contents($flux));
+
+				$ok[$uid] = "ok";
+		}
+	}
+	print_r($ok);
+
+?>
